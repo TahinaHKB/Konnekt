0b70c5a6a392d1aaa79e30df20a6f25e7178bb7f
HKB <165798571+TahinaHKB@users.noreply.github.com>
2025-11-07 21:06:42 +0100

    Update Login.tsx

--- src/page/Login.tsx
+++ src/page/Login.tsx
@@ -2,17 +2,21 @@ import { useState, type FormEvent } from "react";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase";
 import { Link, useNavigate } from "react-router-dom";
+import Loading from "../component/LoadingComment";
 
 export default function LoginPage() {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [error, setError] = useState<string>("");
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
 
   const handleLogin = async (e: FormEvent) => {
     e.preventDefault();
     try {
+      setLoading(true);
       await signInWithEmailAndPassword(auth, email, password);
+      setLoading(false);
       navigate("/home");
     } catch (err: any) {
       setError(err.message);
@@ -22,7 +26,9 @@ export default function LoginPage() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-green-200 to-blue-500 px-4">
       <div className="max-w-md w-full bg-white rounded-xl shadow-lg p-8 space-y-6">
-        <h2 className="text-2xl font-bold text-gray-800 text-center">Se connecter</h2>
+        <h2 className="text-2xl font-bold text-gray-800 text-center">
+          Se connecter
+        </h2>
         <form className="space-y-4" onSubmit={handleLogin}>
           <div>
             <label className="block text-gray-700 mb-1">Email</label>
@@ -47,20 +53,25 @@ export default function LoginPage() {
             />
           </div>
           {error && <p className="text-red-500 text-sm">{error}</p>}
-          <button
-            type="submit"
-            className="w-full bg-blue-500 text-white py-2 rounded-lg font-semibold hover:bg-blue-600 transition duration-300"
-          >
-            Se connecter
-          </button>
+          {loading ? (
+            <Loading msg="Connexion..." />
+          ) : (
+            <button
+              type="submit"
+              className="w-full bg-blue-500 text-white py-2 rounded-lg font-semibold hover:bg-blue-600 transition duration-300"
+            >
+              Se connecter
+            </button>
+          )}
         </form>
         <p className="text-center text-gray-500 text-sm">
-          Pas de compte ? <Link
-        to="/register"
-        className="text-purple-500 font-semibold hover:underline"
-      >
-        Inscrivez-vous
-      </Link>
+          Pas de compte ?{" "}
+          <Link
+            to="/register"
+            className="text-purple-500 font-semibold hover:underline"
+          >
+            Inscrivez-vous
+          </Link>
         </p>
       </div>
     </div>
